82be9d1113310326e62c51f0f85b35fd19c6848b
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-08 01:38:16 +0600

    news details complete

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
-import Home from "../pages/Home/Home/Home";
-import Category from "../pages/Home/Category/Category";
 import NewsLayout from "../layouts/NewsLayout";
+import Category from "../pages/Home/Category/Category";
+import Home from "../pages/Home/Home/Home";
 import News from "../pages/News/News";
-
 
 const router = createBrowserRouter([
 	{
@@ -18,7 +17,8 @@ const router = createBrowserRouter([
 			{
 				path: "/category/:id",
 				element: <Category></Category>,
-				loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`),
+				loader: ({ params }) =>
+					fetch(`http://localhost:5000/categories/${params.id}`),
 			},
 		],
 	},
@@ -29,9 +29,11 @@ const router = createBrowserRouter([
 			{
 				path: ":id",
 				element: <News></News>,
+				loader: ({ params }) =>
+					fetch(`http://localhost:5000/news/${params.id}`),
 			},
 		],
 	},
 ]);
 
-export default router
+export default router;
