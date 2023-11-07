22ddae5cfe5064cf2bc5f885e32b37f3cf5c92ed
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-07 21:53:02 +0600

    left navbar data loaded

--- src/pages/Share/LeftNav/LeftNav.jsx
+++ src/pages/Share/LeftNav/LeftNav.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    return (
-        <div>
-            <h2>Left Nav</h2>
-        </div>
-    );
+	const [categories, setCategories] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:5000/categories")
+			.then((res) => res.json())
+			.then((data) => setCategories(data))
+			.catch((error) => console.log(error));
+	}, []);
+	return (
+		<div>
+			<h4>All Category</h4>
+			<div className="space-y-6">
+				<h2 className="text-2xl">All Categories</h2>
+				{categories.map((category) => (
+					<p key={category.id} className="text-black text-decoration-none" >
+						<Link
+							
+							key={category.id}
+							to={`/category/${category.id}`}>
+							{category.name}
+						</Link>
+					</p>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default LeftNav;
--- src/pages/Share/RightNav/RightNav.jsx
+++ src/pages/Share/RightNav/RightNav.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
+import QZone from "../QZone/QZone";
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>Right Nav</h2>
-        </div>
-    );
+	return (
+		<div>
+			<h4>Login With</h4>
+			<Button
+				variant="outline-primary"
+				className="mb-2 d-flex align-items-center justify-content-between">
+				<FaGoogle /> Login with Google
+			</Button>
+			<Button
+				variant="outline-secondary"
+				className="d-flex align-items-center justify-content-between">
+				<FaGithub /> Login with Github
+			</Button>
+			<div>
+				<ListGroup>
+					<ListGroup.Item> <FaFacebook/> facebook</ListGroup.Item>
+					<ListGroup.Item><FaTwitter/> twitter</ListGroup.Item>
+					<ListGroup.Item><FaInstagram/> instagram</ListGroup.Item>
+				</ListGroup>
+			</div>
+			<QZone></QZone>
+		</div>
+	);
 };
 
 export default RightNav;
