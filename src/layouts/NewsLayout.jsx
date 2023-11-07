8928ca2da2f7933b49064d37f3fb92be029f26df
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-08 01:14:00 +0600

    category wise news completed

--- src/layouts/NewsLayout.jsx
+++ src/layouts/NewsLayout.jsx
@@ -1,4 +1,3 @@
-
 import { Col, Container, Row } from "react-bootstrap";
 import Footer from "../pages/Share/Footer/Footer";
 import Header from "../pages/Share/Header/Header";
@@ -6,27 +5,24 @@ import LeftNav from "../pages/Share/LeftNav/LeftNav";
 import RightNav from "../pages/Share/RightNav/RightNav";
 import { Outlet } from "react-router-dom";
 
-const Main = () => {
+const NewsLayout = () => {
 	return (
-		<>
+		<div>
 			<Header></Header>
 			<Container>
-				
 				<Row>
-                    <Col lg={3}>
-                        <LeftNav></LeftNav>
-                    </Col>
-                    <Col lg={6}>
-                        <Outlet></Outlet>
-                    </Col>
-                    <Col lg={3}>
-                        <RightNav></RightNav>
-                    </Col>
+					
+					<Col lg={9}>
+						<Outlet></Outlet>
+					</Col>
+					<Col lg={3}>
+						<RightNav></RightNav>
+					</Col>
 				</Row>
 			</Container>
 			<Footer></Footer>
-		</>
+		</div>
 	);
 };
 
-export default Main;
+export default NewsLayout;
