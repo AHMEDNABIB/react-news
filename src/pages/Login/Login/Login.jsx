fb37dece962e395a58140c9208c6c2474a51ba64
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-08 19:36:13 +0600

    registration and login with firebase added

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AuthContext } from "./../../../providers/AuthProvider";
 
 const Login = () => {
+	const { signIn } = useContext(AuthContext);
+	const handleRegister = (event) => {
+		event.preventDefault();
+		const form = event.target;
+
+		const email = form.email.value;
+		const password = form.password.value;
+
+		console.log(email, password);
+		signIn(email, password)
+			.then((result) => {
+				const createdUser = result.user;
+				console.log(createdUser);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 	return (
 		<Container className="w-25 mx-auto">
 			<h3>Please Login</h3>
-			<Form>
+			<Form onSubmit={handleRegister}>
 				<Form.Group className="mb-3" controlId="formBasicEmail">
 					<Form.Label>Email address</Form.Label>
 					<Form.Control
@@ -26,9 +45,7 @@ const Login = () => {
 						required
 					/>
 				</Form.Group>
-				<Form.Group className="mb-3" controlId="formBasicCheckbox">
-					<Form.Check type="checkbox" label="Check me out" />
-				</Form.Group>
+			
 				<Button variant="primary" type="submit">
 					Login
 				</Button>
